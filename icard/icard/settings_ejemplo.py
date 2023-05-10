9ddea5939f9877588f35e965f81f14af4b19346e
Diego Herrera <daherre0@gmail.com>
2023-05-10 18:37:56 -0500

    settings_ejemplo.py para postgres y se agrega requirements.txt

--- icard/icard/settings_ejemplo.py
+++ icard/icard/settings_ejemplo.py
@@ -77,10 +77,23 @@ WSGI_APPLICATION = 'icard.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'icard2',
+
+        'USER': 'postgres',
+
+        'PASSWORD': '*****',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
+
     }
+
 }
 
 
