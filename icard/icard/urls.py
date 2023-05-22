f587241c8312e5146afef0271ff3322e4ec89639
Sharid Madrid <sharid.madrid@udea.edu.co>
2023-05-22 17:15:53 -0500

    renderizar tabla de categoria

--- icard/icard/urls.py
+++ icard/icard/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-
+from django.conf.urls.static import static
 from categories.api.router import router_category
 
 schema_view = get_schema_view(
@@ -42,3 +43,4 @@ urlpatterns = [
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('api/', include(router_category.urls)),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
