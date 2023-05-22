4f5b16c7a18c2fd30cd70eefc53ee318102fe3fd
Sharid Madrid <sharid.madrid@udea.edu.co>
2023-05-22 18:42:16 -0500

    crud categorias

--- icard/urls.py
+++ icard/urls.py
@@ -21,7 +21,11 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from django.conf.urls.static import static
+
+
 from categories.api.router import router_category
+from products.api.router import router_product
+
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -42,5 +46,6 @@ urlpatterns = [
     path('redocs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('api/', include(router_category.urls)),
+    path('api/', include(router_product.urls)),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
