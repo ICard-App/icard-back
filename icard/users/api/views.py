49ca2fa47f30597bb2c47b5785a1cfd6fa76643a
Diego Herrera <daherre0@gmail.com>
2023-05-22 23:04:22 -0500

    Se configura para encriptar contraseña enviada al crear usuarios

--- icard/users/api/views.py
+++ icard/users/api/views.py
@@ -2,9 +2,14 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAdminUser
 from users.models import User
 from users.api.serializers import UserSerializer
+from django.contrib.auth.hashers import make_password
 
 
 class UserApiViewSet(ModelViewSet):
     permission_classes = [IsAdminUser]
     serializer_class = UserSerializer
     queryset = User.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        request.data['password'] = make_password(request.data['password'])
+        return super().create(request, *args, **kwargs)
